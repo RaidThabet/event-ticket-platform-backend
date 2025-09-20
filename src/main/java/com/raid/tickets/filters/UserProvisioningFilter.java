6ff1f9f37494c70dcd49d7db26d8757ee0166b85
Raid Thabet <thabet.raid123@gmail.com>
2025-09-20 16:45:18 +0100

    feat: remove unused import for NonNullApi in UserProvisioningFilter

--- src/main/java/com/raid/tickets/filters/UserProvisioningFilter.java
+++ src/main/java/com/raid/tickets/filters/UserProvisioningFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.lang.NonNullApi;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
