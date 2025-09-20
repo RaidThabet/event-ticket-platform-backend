ab7a0f979673f1b2db45fdef9f459c2921a7e44f
Raid Thabet <thabet.raid123@gmail.com>
2025-09-20 19:41:35 +0100

    feat: add DTOs for event and ticket type creation with validation

---
+++ src/main/java/com/raid/tickets/domain/dtos/CreateTicketTypeRequestDto.java
@@ -0,0 +1,27 @@
+package com.raid.tickets.domain.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateTicketTypeRequestDto {
+
+    @NotBlank(message = "Ticket type name is required")
+    private String name;
+
+    private Integer totalAvailable;
+
+    @NotNull(message = "Price is required")
+    @PositiveOrZero(message =  "Price must be zero or greater")
+    private Double price;
+
+    private String description;
+}
