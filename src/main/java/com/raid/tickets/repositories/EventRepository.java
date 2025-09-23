15b132bd1766f81c9fb4f6a8f1140fa0afda3147
Raid Thabet <thabet.raid123@gmail.com>
2025-09-23 23:18:48 +0100

    refactor: remove unused imports in EventRepository and EventServiceImpl

--- src/main/java/com/raid/tickets/repositories/EventRepository.java
+++ src/main/java/com/raid/tickets/repositories/EventRepository.java
@@ -4,7 +4,6 @@ import com.raid.tickets.domain.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.parameters.P;
 
 import java.util.UUID;
 
