d5a62610859eab16d3d005627f1a3166de93607f
Raid Thabet <thabet.raid123@gmail.com>
2025-09-24 19:45:58 +0100

    feat: add method to retrieve event by ID for a specific organizer

--- src/main/java/com/raid/tickets/services/EventService.java
+++ src/main/java/com/raid/tickets/services/EventService.java
@@ -5,9 +5,11 @@ import com.raid.tickets.domain.request.CreateEventRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EventService {
     Event createEvent(UUID organizerId, CreateEventRequest event);
     Page<Event> listEventsForOrganizer(UUID organizeId, Pageable pageable);
+    Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
 }
