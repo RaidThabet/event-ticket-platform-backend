1a6a8545bea42fb85ed5fc6771ed08366f34570e
Raid Thabet <thabet.raid123@gmail.com>
2025-09-26 21:33:04 +0100

    feat: add DTO for listing published events and update EventMapper

---
+++ src/main/java/com/raid/tickets/domain/dtos/ListPublishedEventResponseDto.java
@@ -0,0 +1,24 @@
+package com.raid.tickets.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListPublishedEventResponseDto {
+
+    private UUID id;
+
+    private String name;
+
+    private LocalDateTime startDateTime;
+
+    private LocalDateTime endDateTime;
+
+    private String venue;
+}
